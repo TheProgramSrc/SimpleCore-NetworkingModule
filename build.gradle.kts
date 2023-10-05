7c0588a27af746709dab018893fd734d256e8fa6
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2023-10-05 07:45:52 -0300

    feat: comply with `simplecoreapi` 0.8.0 (#34)
    * Updated some dependencies
    * Migrated to new module system

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,18 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     `maven-publish`
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
-    id("com.github.johnrengelman.shadow") version "8.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("cl.franciscosolis.blossom-extended") version "1.3.1"
 
-    kotlin("jvm") version "1.8.10"
-    id("org.jetbrains.dokka") version "1.8.10"
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.dokka") version "1.9.0"
 }
 
 val env = project.rootProject.file(".env").let { file ->
     if(file.exists()) file.readLines().filter { it.isNotBlank() && !it.startsWith("#") && it.split("=").size == 2 }.associate { it.split("=")[0] to it.split("=")[1] } else emptyMap()
 }.toMutableMap().apply { putAll(System.getenv()) }
 
-val projectVersion = env["VERSION"] ?: "0.2.0-SNAPSHOT"
+val projectVersion = env["VERSION"] ?: "0.3.0-SNAPSHOT"
 
 group = "xyz.theprogramsrc"
 version = projectVersion
@@ -26,19 +26,22 @@ repositories {
     mavenCentral()
 
     maven("https://s01.oss.sonatype.org/content/groups/public/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/releases/")
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.codemc.org/repository/maven-public/")
     maven("https://jitpack.io/")
 }
 
 dependencies {
-    compileOnly("xyz.theprogramsrc:simplecoreapi:0.6.2-SNAPSHOT")
+    compileOnly("xyz.theprogramsrc:simplecoreapi:0.8.0-SNAPSHOT")
 
-    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
-    compileOnly("net.md-5:bungeecord-api:1.19-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-api:1.20-R0.2-SNAPSHOT")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
 
--- src/main/kotlin/xyz/theprogramsrc/networkingmodule/Main.kt
+++
@@ -1,5 +0,0 @@
-package xyz.theprogramsrc.networkingmodule
-
-import xyz.theprogramsrc.simplecoreapi.global.module.Module
-
-class Main: Module()
